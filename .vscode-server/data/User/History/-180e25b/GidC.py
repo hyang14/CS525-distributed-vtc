0ed7044c6ea2e752ab246f42be11fef9f7bd5c2b
maoyifei2013605 <yifeim3@sp25-cs525-0805.cs.illinois.edu>
2025-04-06 01:37:21 -0500

    Initial commit

---
+++ .vscode-server/data/User/History/-180e25b/GidC.py
@@ -0,0 +1,9 @@
+from vllm import LLM, SamplingParams
+
+llm = LLM(model="gpt2", swap_space=2)  # or any HuggingFace model
+sampling_params = SamplingParams(temperature=0.7, max_tokens=100)
+outputs = llm.generate("What is the capital of France?", sampling_params)
+response = outputs[0]
+response_text = response.outputs[0].text
+num_tokens = len(response.outputs[0].token_ids)
+print(outputs[0].outputs[0].text)
---
+++ .vscode-server/data/User/History/-180e25b/UZ3i.py
@@ -0,0 +1,6 @@
+from vllm import LLM, SamplingParams
+
+llm = LLM(model="gpt2", swap_space=2)  # or any HuggingFace model
+sampling_params = SamplingParams(temperature=0.7, max_tokens=100)
+outputs = llm.generate("What is the capital of France?", sampling_params)
+print(outputs[0].outputs[0].text)
